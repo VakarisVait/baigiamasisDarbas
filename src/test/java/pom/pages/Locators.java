69c0c5a11098c1b8565573f4679c307f8990d871
vakar <vakarisvakarisv@gmail.com>
2022-10-13 14:57:14 +0300

    Add geraiButton in Locators

--- src/test/java/pom/pages/Locators.java
+++ src/test/java/pom/pages/Locators.java
@@ -7,5 +7,6 @@ public class Locators {
     public static class Zelando {
 
         public static By privacyAgreementPopup = By.xpath("//span[@id='uc-full-optin-description']");
+        public static By geraiButton = By.xpath("//button[@id='uc-btn-accept-banner']");
     }
 }
