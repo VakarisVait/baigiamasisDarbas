bb0d2c82eb9bdb3263f221f379e91ffaf8283568
vakar <vakarisvakarisv@gmail.com>
2022-10-13 14:44:04 +0300

    Add privacyAgreementPopup xpath in Locators

--- src/test/java/pom/pages/Locators.java
+++ src/test/java/pom/pages/Locators.java
@@ -1,4 +1,11 @@
 package pom.pages;
 
+import org.openqa.selenium.By;
+
 public class Locators {
+
+    public static class Zelando {
+
+        public static By privacyAgreementPopup = By.xpath("//span[@id='uc-full-optin-description']");
+    }
 }
