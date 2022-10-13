b1494ccc8c0ed5842c633a05f9d554f539ee2557
vakar <vakarisvakarisv@gmail.com>
2022-10-13 14:06:19 +0300

    Common class created

---
+++ src/test/java/pom/pages/Common.java
@@ -0,0 +1,26 @@
+package pom.pages;
+
+import pom.utils.Driver;
+
+public class Common {
+
+    public static void setupDriver() {
+        Driver.setDriver();
+    }
+
+    public static void openUrl(String url) {
+        Driver.getDriver().get(url);
+    }
+
+    public static void closeDriver() {
+        Driver.close();
+    }
+
+    public static void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
