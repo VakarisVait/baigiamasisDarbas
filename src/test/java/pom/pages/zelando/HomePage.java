f7060e4951ca2a21473b79d79f993311b2c038a6
vakar <vakarisvakarisv@gmail.com>
2022-10-13 14:42:58 +0300

    Add acceptPrivacyConfirmation in HomePage

--- src/test/java/pom/pages/zelando/HomePage.java
+++ src/test/java/pom/pages/zelando/HomePage.java
@@ -1,4 +1,17 @@
 package pom.pages.zelando;
 
+import pom.pages.Common;
+import pom.pages.Locators;
+
 public class HomePage {
+    public static void open(String url) {
+        Common.openUrl(url);
+    }
+
+    public static void acceptPrivacyConfirmation() {
+        Common.waitForElementToAppear(
+                Locators.Zelando.privacyAgreementPopup
+        );
+        Common.clickElement();
+    }
 }
