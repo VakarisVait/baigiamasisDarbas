42418ecc557a7c42774212ac4e1a3296016cd533
vakar <vakarisvakarisv@gmail.com>
2022-10-14 13:23:15 +0300

    Added a test to check clothing colors in Locators

--- src/test/java/pom/tests/zelando/HomeVaikamsTest.java
+++ src/test/java/pom/tests/zelando/HomeVaikamsTest.java
@@ -1,4 +1,34 @@
 package pom.tests.zelando;
 
-public class HomeVaikamsTest {
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pom.pages.zelando.HomeVaikamsPage;
+import pom.tests.TestBase;
+
+public class HomeVaikamsTest extends TestBase {
+    @BeforeMethod
+    @Override
+    public void setUp() {
+        super.setUp();
+        HomeVaikamsPage.open("https://www.zalando.lt/vaikams-drabuziai-laisvos-kelnes" +
+                "/?q=Lined+pull+on+cod+kelnes+navy+blue");
+
+        HomeVaikamsPage.acceptPrivacyConfirmation();
+    }
+
+    @Test
+    public void optionToChangeClothingColor() {
+        String expectedColor = "brown";
+        String actualColor = "";
+
+        HomeVaikamsPage.clickClothingPicture();
+        HomeVaikamsPage.clickDifferentColorOptionPicture();
+
+        actualColor = HomeVaikamsPage.readValueOfColorText();
+
+        Assert.assertTrue(actualColor.contains(expectedColor));
+
+        HomeVaikamsPage.sleep(3000);
+    }
 }
