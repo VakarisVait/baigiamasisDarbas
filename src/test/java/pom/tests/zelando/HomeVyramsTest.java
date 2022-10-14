8ec36fbf1ce40fb4b52128b2276c78db89af0390
vakar <vakarisvakarisv@gmail.com>
2022-10-14 12:13:11 +0300

    Added productFilteringOption test in HomeVyrams

--- src/test/java/pom/tests/zelando/HomeVyramsTest.java
+++ src/test/java/pom/tests/zelando/HomeVyramsTest.java
@@ -1,4 +1,34 @@
 package pom.tests.zelando;
 
-public class HomeVyramsTest {
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pom.pages.zelando.HomeVyramsPage;
+import pom.tests.TestBase;
+
+public class HomeVyramsTest extends TestBase {
+
+    @BeforeMethod
+    @Override
+    public void setUp() {
+        super.setUp();
+        HomeVyramsPage.open("https://www.zalando.lt/vyrams-home/");
+        HomeVyramsPage.acceptPrivacyConfirmation();
+    }
+
+    @Test
+    public void productFilteringOption(){
+        String expectedFilterText = "Padai ir vidpadiniai įdėklai vyrams";
+        String actualFilterText = "";
+
+        HomeVyramsPage.clickAvalyneButton();
+        HomeVyramsPage.clickAvalynesAksesuaraiButton();
+        HomeVyramsPage.clickPadaiIrVidpadiniaiIdeklaiButton();
+        HomeVyramsPage.sleep(2000);
+        actualFilterText = HomeVyramsPage.readValueOfText();
+
+        Assert.assertEquals(actualFilterText, expectedFilterText);
+
+        HomeVyramsPage.sleep(2000);
+    }
 }
