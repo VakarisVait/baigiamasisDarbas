d882610b308e9c17b1607feab74dae43fd02a61f
vakar <vakarisvakarisv@gmail.com>
2022-10-14 14:50:42 +0300

    Added test for add function to bag VyramsClothingInnerPage

--- src/test/java/pom/pages/zelando/VyramsClothingInnerPage.java
+++ src/test/java/pom/pages/zelando/VyramsClothingInnerPage.java
@@ -1,4 +1,41 @@
 package pom.pages.zelando;
 
+import pom.pages.Common;
+import pom.pages.Locators;
+
 public class VyramsClothingInnerPage {
+    public static void open(String url) {
+        Common.openUrl(url);
+    }
+
+    public static void sleep(int millis) {
+        Common.sleep(millis);
+    }
+
+    public static void acceptPrivacyConfirmation() {
+        Common.waitForElementToAppear(
+                Locators.Zelando.HomeMoterims.privacyAgreementPopup
+        );
+        Common.clickElement(
+                Locators.Zelando.HomeMoterims.geraiButton
+        );
+    }
+
+    public static void clickPridetiIKrepseliButton() {
+        Common.clickElement(
+                Locators.Zelando.VyramsClothingInner.pridetiIKrepseliButton
+        );
+    }
+
+    public static void clickBagIconButton() {
+        Common.clickElement(
+                Locators.Zelando.VyramsClothingInner.bagIconButton
+        );
+    }
+
+    public static String readValueOfItemInsideBagText() {
+        return Common.getElementText(
+                Locators.Zelando.VyramsClothingInner.valueOfItemInsideBagText
+        );
+    }
 }
