467999d00a607ca72572bbed6306cdcb195eac14
vakar <vakarisvakarisv@gmail.com>
2022-10-14 14:51:44 +0300

    Added test for function add to bag VyramsClothingInnerTest

--- src/test/java/pom/tests/zelando/VyramsClothingInnerTest.java
+++ src/test/java/pom/tests/zelando/VyramsClothingInnerTest.java
@@ -1,8 +1,38 @@
 package pom.tests.zelando;
 
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+import pom.pages.zelando.HomeVyramsPage;
+import pom.pages.zelando.VyramsClothingInnerPage;
+import pom.tests.TestBase;
 import pom.utils.TestListener;
 
 @Listeners(TestListener.class)
-public class VyramsClothingInnerTest {
+public class VyramsClothingInnerTest extends TestBase {
+
+    @BeforeMethod
+    @Override
+    public void setUp() {
+        super.setUp();
+        VyramsClothingInnerPage.open("https://www.zalando.lt/versace-jeans-couture-unisex-" +
+                "kepuraite-blackgold-vei54q010-q11.html");
+        VyramsClothingInnerPage.acceptPrivacyConfirmation();
+    }
+
+    @Test
+    public void addItemToBag() {
+        String expectedText = "Versace Jeans Couture";
+        String actualText = "";
+
+        VyramsClothingInnerPage.clickPridetiIKrepseliButton();
+        VyramsClothingInnerPage.clickBagIconButton();
+        HomeVyramsPage.sleep(2000);
+        actualText = VyramsClothingInnerPage.readValueOfItemInsideBagText();
+
+        Assert.assertTrue(actualText.contains(expectedText));
+
+        HomeVyramsPage.sleep(3000);
+    }
 }
