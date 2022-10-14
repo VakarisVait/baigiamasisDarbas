a4087d3839afe498c379b249a9a3f839cfbd81d1
vakar <vakarisvakarisv@gmail.com>
2022-10-14 14:49:02 +0300

    Added xpath to classes inside VyramsClothingInner

--- src/test/java/pom/pages/Locators.java
+++ src/test/java/pom/pages/Locators.java
@@ -61,16 +61,21 @@ public class Locators {
 
             public static By differentColorOptionPicture = By.xpath("(//div[@class='JT3_zV mo6ZnF'])[4]");
 
-            public static By valueOfColorText = By.xpath("//p[@class='_0Qm8W1 u-6V88 dgII7d pVrzNP zN9KaA']");
+            public static By valueOfColorText = By.xpath("//p[@class='_0Qm8W1 u-6V88 dgII7d pVrzNP" +
+                    " zN9KaA']");
         }
 
         public static class VyramsClothingInner {
 
-            public static By pridetiIKrepseliButton = By.xpath("");
+            public static By pridetiIKrepseliButton = By.xpath("//button[@class='DJxzzA u9KIT8 uEg2FS" +
+                    " U_OhzR ZkIJC- Vn-7c- FCIprz heWLCX JIgPn9 LyRfpJ pxpHHp Md_Vex NN8L-8 GTG2H9 MfX1a0 WCjo-q" +
+                    " EKabf7 aX2-iv r9BRio mo6ZnF  E6Km4r']");
 
-            public static By bagIconButton = By.xpath("");
+            public static By bagIconButton = By.xpath("(//div[@class='z-navicat-header_navToolCounter" +
+                    " JT3_zV DJxzzA'])[2]");
 
-            public static By valueOfItemInsideBagText = By.xpath("");
+            public static By valueOfItemInsideBagText = By.xpath("//div[@class='z-coast-base__article__" +
+                    "details-group']");
         }
     }
 }
